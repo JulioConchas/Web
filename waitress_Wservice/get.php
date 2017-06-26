36948203f948637f6018bdeb815cfe030f7aef96
Julio Cesar Conchas Jimenez <julio@oc0025030637.ibm.com>
2017-06-26 10:29:41 -0500

    waitres virtual web service rest full a

---
+++ waitress_Wservice/get.php
@@ -0,0 +1,27 @@
+<?php
+	
+	//Conchas Jimenez Julio Cesar
+	//conchasjimenez@gmail.com
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    if ($id = $_GET['id']) {
+        $conn = mysqli_connect('localhost', 'root', '', 'webservice');
+        //var_dump($conn);
+        $sql = "
+                    SELECT * FROM user WHERE id='$id'
+               ";
+        if (mysqli_connect_errno()) {
+            echo json_encode(array(
+                'status' => 'failure',
+                'message' => 'Could Not connect to database',
+            ));
+        }
+        $data = mysqli_query($conn, $sql);
+        if ($data) {
+            while ($row = mysqli_fetch_array($data)) {
+                echo json_encode($row);
+            }
+        }
+    }
+}
+?>
